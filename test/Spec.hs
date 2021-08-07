e7afd11f8bb566c7009692296c1b7b6dfe2e05ec
michaelrauh <michaelrauh@ymail.com>
2021-08-07 10:10:31 -0400

    add dummy test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
+import Test.HUnit
+import System.Exit
+
+testSum = TestCase $ assertEqual "10 + 5 = 15" 15 (10 + 5)
+
+testProd = TestCase $ assertEqual "10 * 15" 150 (10 * 15)
+
+testPred = TestCase $ assertBool "10 > 5" (10 > 5)
+
+testFailure = TestCase $ assertEqual "It will fail 10 + 2 = 15" (10 + 2) 15  
+
+testlist = TestList [TestLabel "testSum" testSum,
+                     TestLabel "testPred" testPred,
+                     TestLabel "testFailure" testFailure,
+                     TestLabel "testProd" testProd                    
+                    ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  results <- runTestTT testlist
+  if errors results + failures results == 0
+    then
+      exitSuccess
+    else
+      exitWith (ExitFailure 1)
