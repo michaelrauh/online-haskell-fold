935e6139526ae50227a541b3d0da1669f5d95d77
michaelrauh <michaelrauh@ymail.com>
2021-08-08 11:56:10 -0400

    add config to hold text-scoped data

---
+++ src/Config.hs
@@ -0,0 +1,18 @@
+module Config
+  ( makeConfig
+  )
+where
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import ConfigUtil
+    ( makeNextMapping, makePrevMapping, makePhrases, makeVocabulary )
+
+data Config = Config {
+    next :: Map.Map String (Set.Set String),
+    prev :: Map.Map String (Set.Set String),
+    phrases :: Set.Set [String],
+    vocab :: [String]
+} deriving Show
+
+makeConfig :: String -> Config
+makeConfig s = Config {next=makeNextMapping s, prev=makePrevMapping s, phrases=makePhrases s, vocab=makeVocabulary s}
