be1c35907c586c18264a68dcf794b77399d252af
michaelrauh <michaelrauh@ymail.com>
2021-08-08 20:40:19 -0400

    change direction on ortho representation. Use map of nodes

--- src/Ortho.hs
+++ src/Ortho.hs
@@ -1,46 +1,63 @@
-module Ortho (eatWord, origin, dimensions, hop) where
+module Ortho (eatWord, getOrigin, getDimensions) where
 
 import Config (Config (Config))
 import Data.List (group, sort)
 import Data.Set as Set (Set, fromList)
+import Data.Map as Map (Map, singleton, fromList, empty, lookup, keys)
 import WordEater ( Answer(..), wordToUniqueAnswer )
+import Data.List.NonEmpty (cons)
 
-newtype Ortho = Ortho {nodes :: [Node]} -- todo add entry points list that index into nodes. Rename nodes getNodes
+newtype Ortho = Ortho{origin :: Node}
 
 data Node = Node
   { name :: String,
-    distance :: Integer
+    distance :: Integer,
+    unravelNeighbor :: Map.Map String Node
   }
 
 eatWord :: Config -> String -> [Ortho]
-eatWord conf cur = map fromAnswer $ wordToUniqueAnswer conf cur
+eatWord conf cur = fromAnswer <$> wordToUniqueAnswer conf cur
 
 fromAnswer :: Answer -> Ortho
-fromAnswer (Answer a b c d) = Ortho [Node a 0, Node b 1, Node c 1, Node d 2]
+fromAnswer (Answer a b c d) = let
+  nodeA = Node a 0 (Map.fromList [("b", nodeB), ("c", nodeC)])
+  nodeB = Node b 1 (Map.fromList [("b", nodeC), ("c", nodeD)])
+  nodeC = Node c 1 (Map.fromList [("b", nodeD), ("c", nodeB)])
+  nodeD = Node d 2 Map.empty
+  in Ortho nodeA
 
-origin :: Ortho -> String
-origin = name . head . nodes
+getOrigin :: Ortho -> String
+getOrigin = name . origin
 
-dimensions :: Ortho -> [Int]
-dimensions (Ortho nodes) = map length $ group $ sort $ map distance nodes
+getDimensions :: Ortho -> [Int]
+getDimensions (Ortho origin)= let
+  hopDirection = (head . Map.keys . unravelNeighbor) origin
+  numbers = go origin hopDirection
+  in map length $ group $ sort numbers
 
-hop :: Ortho -> Set.Set String
-hop (Ortho nodes) = Set.fromList $ map name $ filter ((1 ==) . distance) nodes
+go :: Node -> String -> [Integer]
+go (Node _ distance neighbors) hopDirection = 
+  case Map.lookup hopDirection neighbors of Nothing -> [distance]
+                                            Just node -> distance : go node hopDirection
 
-isBase :: Ortho -> Bool 
-isBase = error "Not Implemented"
 
-unravel :: Ortho -> String -> [String]
-unravel = error "Not Implemented"
+-- hop :: Ortho -> Set.Set String
+-- hop (Ortho nodes) = Set.fromList $ name <$> filter ((1 ==) . distance) nodes
 
-diagonals :: Ortho -> [Set.Set String]
-diagonals = error "Not Implemented"
+-- isBase :: Ortho -> Bool
+-- isBase = error "Not Implemented"
 
-lhsCenter :: Ortho -> String -> [String]
-lhsCenter = error "Not Implemented"
+-- unravel :: Ortho -> String -> [String]
+-- unravel = error "Not Implemented"
 
-rhsCenter :: Ortho -> String -> [String]
-rhsCenter = error "Not Implemented"
+-- diagonals :: Ortho -> [Set.Set String]
+-- diagonals = error "Not Implemented"
 
-phrases :: Ortho -> [[String]]
-phrases = error "Not Implemented"
+-- lhsCenter :: Ortho -> String -> [String]
+-- lhsCenter = error "Not Implemented"
+
+-- rhsCenter :: Ortho -> String -> [String]
+-- rhsCenter = error "Not Implemented"
+
+-- phrases :: Ortho -> [[String]]
+-- phrases = error "Not Implemented"
