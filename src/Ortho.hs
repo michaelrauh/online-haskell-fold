6db28220b7952e95e73b70b4f120f3257acccfc9
michaelrauh <michaelrauh@ymail.com>
2021-08-17 16:47:04 -0400

    move orthos into ortho for now and make most of the functions that were in ortho private

--- src/Ortho.hs
+++ src/Ortho.hs
@@ -1,19 +1,31 @@
-module Ortho (Ortho (..), eatWord, fromAnswer, getOrigin, getDimensions, hop, getDiagonals) where
-
-import Config (Config (Config))
+module Ortho (Ortho (..), eatWord, Ortho.size, Ortho.empty, insert, findWithMatchingDimsAndOriginProjectingForward, findWithMatchingDimsAndOriginProjectingBackward, findWithMatchingDimsAndHopToOrigin, findWithMatchingDimsAndOriginToHop) where
 import Data.List (group, sort, groupBy, subsequences, nubBy, sortBy)
-import Data.Set as Set (Set, fromList, toList, size)
-import Data.Map as Map (Map, singleton, fromList, empty, lookup, keys, (!), elems)
+import Data.Set as Set
+    ( Set,
+      fromList,
+      toList,
+      size,
+      Set,
+      empty,
+      fromList,
+      singleton,
+      toList,
+      union,
+      unions )
+import Data.Map as Map (Map, singleton, fromList, empty, lookup, keys, (!), elems, size)
 import WordEater ( Answer(..), wordToUniqueAnswer )
 import Data.Function (on)
+import Config
+import qualified Data.Map as Map (Map, empty, findWithDefault, insertWith)
 
-newtype Ortho = Ortho{origin :: Node}
-
+newtype Ortho = Ortho{origin :: Node} deriving (Eq, Ord, Show)
+data NodeName = Origin String | Hop String deriving (Show, Eq, Ord)
+newtype Orthos = Orthos (Map.Map ([Int], NodeName) (Set.Set Ortho)) deriving (Show)
 data Node = Node
   {
     name :: String,
     neighbors :: Map.Map Int Node
-  }
+  } deriving (Eq, Ord, Show)
 
 eatWord :: Config -> String -> [Ortho]
 eatWord conf cur = fromAnswer <$> wordToUniqueAnswer conf cur
@@ -46,9 +58,8 @@ getDiagonalPaths :: Ortho -> [[Int]]
 getDiagonalPaths ortho = let
   pathToBottomRightCorner = findPathToBottomRightCorner ortho
   paths = subsequences pathToBottomRightCorner
-  uniques = nubBy (\x y -> sort x == sort y) paths
-  comparator = (compare `on` length)
-  answer = sortBy comparator uniques
+  uniques = nubBy ((==) `on` sort) paths
+  answer = sortBy (compare `on` length) uniques
   in answer
 
 findPathToBottomRightCorner :: Ortho -> [Int]
@@ -72,3 +83,67 @@ distanceToEdge (Node _ neighbors) direction = case Map.lookup direction neighbor
 go :: Node -> [Int] -> String
 go (Node name _) [] = name
 go (Node _ neighbors) (f : rest) = go (neighbors Map.! f) rest
+
+empty :: Orthos
+empty = Orthos Map.empty
+
+insert :: Orthos -> Ortho -> Orthos
+insert (Orthos orthoMap) ortho =
+  let dims = getDimensions ortho
+      origin = Origin $ getOrigin ortho
+      originKey = (dims, origin, ortho)
+      hopKeys = Hop <$> Set.toList (hop ortho)
+      missingKeys = originKey : zip3 (repeat dims) hopKeys (repeat ortho)
+      updatedMap = foldr addKeyToMap orthoMap missingKeys
+   in Orthos updatedMap
+
+size :: Orthos -> Int
+size (Orthos m)= Map.size m
+
+addKeyToMap :: ([Int], NodeName, Ortho) -> Map.Map ([Int], NodeName) (Set Ortho) -> Map.Map ([Int], NodeName) (Set Ortho)
+addKeyToMap (dims, nodeName, ortho) = Map.insertWith Set.union (dims, nodeName) (Set.singleton ortho)
+
+findWithMatchingDimsAndOriginProjectingForward :: Config -> Orthos -> Ortho -> Set.Set Ortho
+findWithMatchingDimsAndOriginProjectingForward config = lookupHelper (next config)
+
+findWithMatchingDimsAndOriginProjectingBackward :: Config -> Orthos -> Ortho -> Set.Set Ortho
+findWithMatchingDimsAndOriginProjectingBackward config = lookupHelper (prev config)
+
+lookupHelper :: Map.Map String (Set.Set String)  -> Orthos -> Ortho -> Set.Set Ortho
+lookupHelper mapping (Orthos orthoMap) ortho = let
+    projectedOrigin = Map.findWithDefault Set.empty (getOrigin ortho) mapping
+    dims = getDimensions ortho
+    originKeys = Origin <$> Set.toList projectedOrigin
+    missingKeys = zip (repeat dims) originKeys
+    results = (\key -> Map.findWithDefault Set.empty key orthoMap) <$> missingKeys
+   in Set.unions results
+
+findWithMatchingDimsAndHopToOrigin :: Orthos -> Ortho -> Set.Set Ortho
+findWithMatchingDimsAndHopToOrigin (Orthos orthoMap) ortho =
+  let dims = getDimensions ortho
+      hops = Set.toList (hop ortho)
+      hopKeys = Origin <$> hops
+      missingKeys = zip (repeat dims) hopKeys
+      results = (\key -> Map.findWithDefault Set.empty key orthoMap) <$> missingKeys
+   in Set.unions results
+
+findWithMatchingDimsAndOriginToHop :: Orthos -> Ortho -> Set.Set Ortho
+findWithMatchingDimsAndOriginToHop (Orthos orthoMap) ortho =
+  let dims = getDimensions ortho
+      orthoOrigin = getOrigin ortho
+      hopKeys = Hop orthoOrigin
+      missingKeys = (dims, hopKeys)
+      results = Map.findWithDefault Set.empty missingKeys orthoMap
+   in results
+
+checkDiagonals :: Ortho -> Ortho -> Bool
+checkDiagonals l r = undefined
+
+checkEachPositionProjects :: Config -> Ortho -> Ortho -> Bool
+checkEachPositionProjects config l r = undefined
+
+checkCentersMatch :: Ortho -> Ortho -> Bool
+checkCentersMatch l r = undefined
+
+checkPhrases :: Config -> Ortho -> Ortho -> Bool
+checkPhrases config l r = undefined
--- test/OrthoSpec.hs
+++ test/OrthoSpec.hs
@@ -4,26 +4,58 @@ import Config (makeConfig)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Test.Hspec (Spec, describe, it, shouldBe)
-import Ortho (eatWord, fromAnswer, getDiagonals, getDimensions, getOrigin, hop)
+import Ortho (eatWord, empty, findWithMatchingDimsAndHopToOrigin, findWithMatchingDimsAndOriginProjectingBackward, findWithMatchingDimsAndOriginProjectingForward, findWithMatchingDimsAndOriginToHop, insert, size)
 
 {-# ANN module "HLint: ignore Redundant do" #-}
+{-# ANN module "HLint: ignore Reduce duplication" #-}
 
 spec :: Spec
 spec = do
   describe "Ortho" $ do
-    it "exposes origin through ortho" $ do
-      let result = getOrigin $ head $ eatWord (makeConfig "a b c d a c b d") "d"
-          expected = "a"
+    it "creating orthos and inserting them in multiple indices equivalent to dimensionality + 1" $ do
+      let result = size $ insert empty (head $ eatWord (makeConfig "a b c d a c b d") "d")
+          expected = 3
       result `shouldBe` expected
-    it "exposes dimensions through ortho" $ do
-      let result = getDimensions $ head $ eatWord (makeConfig "a b c d a c b d") "d"
-          expected = [1, 2, 1]
+    it "searching for orthos by dims and origin over a projection when looking for RHS" $ do
+      let config = makeConfig "a b c d a c b d a e f g h e g f h"
+          original = head $ eatWord config "d"
+          new = head $ eatWord config "h"
+          store = insert empty new
+          result = findWithMatchingDimsAndOriginProjectingForward config store original
+          expected = Set.singleton new
       result `shouldBe` expected
-    it "allows seeing what is one away from origin" $ do
-      let result = hop $ head $ eatWord (makeConfig "a b c d a c b d") "d"
-          expected = Set.fromList ["b", "c"]
+    it "searching for orthos by dims and origin over a projection when looking for LHS" $ do
+      let config = makeConfig "a b c d a c b d a e f g h e g f h"
+          new = head $ eatWord config "d"
+          original = head $ eatWord config "h"
+          store = insert empty new
+          result = findWithMatchingDimsAndOriginProjectingBackward config store original
+          expected = Set.singleton new
       result `shouldBe` expected
-    it "reveals diagonals" $ do
-      let result = getDiagonals (head $ eatWord (makeConfig "a b c d a c b d") "d")
-          expected = [Set.fromList ["a"], Set.fromList ["c", "b"], Set.fromList ["d"]]
+    it "searching for orthos by dims by matching a hop to an origin RHS" $ do
+      let config = makeConfig "a b c d a c b d b e d f b d e f"
+          new = head $ eatWord config "f"
+          original = head $ eatWord config "d"
+          store = insert empty new
+          result = findWithMatchingDimsAndHopToOrigin store original
+          expected = Set.singleton new
       result `shouldBe` expected
+    it "searching for orthos by dims by matching an origin to a hop LHS" $ do
+      let config = makeConfig "a b c d a c b d b e d f b d e f"
+          original = head $ eatWord config "f"
+          new = head $ eatWord config "d"
+          store = insert empty new
+          result = findWithMatchingDimsAndOriginToHop store original
+          expected = Set.singleton new
+      result `shouldBe` expected
+    it "checks to make sure diagonals don't match up for all rotations. Has a left to right bias" $ do
+      let config = makeConfig "a b c d a c b d b e d f b d e f"
+          original = head $ eatWord config "f"
+          new = head $ eatWord config "d"
+          store = insert empty new
+          result = findWithMatchingDimsAndOriginToHop store original
+          expected = Set.singleton new
+      result `shouldBe` expected
+
+-- a b  
+-- c d  
