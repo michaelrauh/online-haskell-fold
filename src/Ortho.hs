abc11ddb6c9d39ecb648e4dac1a7ccc5dce1c2db
michaelrauh <michaelrauh@ymail.com>
2021-09-01 18:18:47 -0400

    build top layer to drive out additional types

--- src/Ortho.hs
+++ src/Ortho.hs
@@ -19,14 +19,49 @@ newtype DirectedOrthos = DirectedOrthos (Set.Set DirectedOrtho)
 data DirectedOrtho = DirectedOrtho {ortho :: ShiftedOrtho, combineAxis :: Text}
 newtype ShiftedOrtho = ShiftedOrtho Ortho
 
+digestWords :: Config -> Orthos
+digestWords config = Prelude.foldr (digestIt config) emptyOrtho $ vocab config
+
+digestIt :: Config -> String -> Orthos -> Orthos
+digestIt config word orthos = digestWord config orthos $ pack word
+
+digestWord :: Config -> Orthos -> Text -> Orthos
+digestWord config orthos word = sift config orthos $ eatWord config word
+
+sift :: Config -> Orthos -> Orthos -> Orthos
+sift config known increment = if emptyOrthos increment then known else let 
+  (f, rest) = pickOne increment
+  incrementUp = combineIfBase config known f
+  newKnown = mergeOrthos known incrementUp
+  up = sift config newKnown incrementUp
+  incrementOver = combineOver config up f
+  veryNewKnown = mergeOrthos newKnown incrementOver
+  over = sift config veryNewKnown incrementOver
+  in sift config over rest
+
+mergeOrthos :: Orthos -> Orthos -> Orthos
+mergeOrthos = undefined
+
+combineOver :: Config -> Orthos -> Ortho -> Orthos
+combineOver = undefined
+
+pickOne :: Orthos -> (Ortho, Orthos)
+pickOne = undefined
+
+emptyOrthos :: Orthos -> Bool 
+emptyOrthos = undefined
+
+combineIfBase :: Config -> Orthos -> Ortho -> Orthos -- if it is not base return the empty orthos. If it is base return only previously unknown results
+combineIfBase = error "not implemented"
+
 eatWord :: Config -> Text -> Orthos
 eatWord = undefined
 
 fromAnswer :: Answer -> Ortho
 fromAnswer = undefined
 
-empty :: Orthos
-empty = undefined
+emptyOrtho :: Orthos
+emptyOrtho = undefined
 
 insert :: Orthos -> Ortho -> Orthos
 insert = undefined
