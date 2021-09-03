4bd0de3f09707653f05b7adf962c0f3e7fa20444
michaelrauh <michaelrauh@ymail.com>
2021-09-03 19:21:18 -0400

    WIP top down ideas

--- src/Ortho.hs
+++ src/Ortho.hs
@@ -5,7 +5,7 @@ import WordEater (Answer(..), wordToUniqueAnswer )
 import Data.Function (on)
 import Config
 import qualified Data.Map as Map (Map, empty, findWithDefault, insertWith)
-import Data.List (delete)
+import Data.List (delete, sort, maximumBy, nub)
 import Data.Text
 
 newtype Path = Path [Text] deriving (Eq, Ord, Show)
@@ -13,22 +13,22 @@ data Node = Node
   { name :: Text,
     location :: Path
   } deriving (Eq, Ord, Show)
-newtype Ortho = Ortho {nodes :: Set.Set Node}
+newtype Ortho = Ortho {nodes :: Set.Set Node} deriving (Eq, Ord)
 newtype Orthos = Orthos (Set.Set Ortho)
 newtype DirectedOrthos = DirectedOrthos (Set.Set DirectedOrtho)
 data DirectedOrtho = DirectedOrtho {ortho :: ShiftedOrtho, combineAxis :: Text}
 newtype ShiftedOrtho = ShiftedOrtho Ortho
 
 digestWords :: Config -> Orthos
-digestWords config = Prelude.foldr (digestWord config) emptyOrtho $ pack <$> vocab config
+digestWords config = Prelude.foldr (digestWord config) emptyOrtho $ vocab config
 
-digestWord :: Config -> Text -> Orthos -> Orthos
+digestWord :: Config -> String -> Orthos -> Orthos
 digestWord config word orthos = sift config orthos $ eatWord config word
 
 sift :: Config -> Orthos -> Orthos -> Orthos
-sift config known increment = if emptyOrthos increment then known else let 
+sift config known increment = if isEmpty increment then known else let
   (f, rest) = pickOne increment
-  incrementUp = combineIfBase config known f
+  incrementUp = combineUpIfBase config known f
   newKnown = mergeOrthos known incrementUp
   up = sift config newKnown incrementUp
   incrementOver = combineOver config up f
@@ -37,34 +37,72 @@ sift config known increment = if emptyOrthos increment then known else let
   in sift config over rest
 
 mergeOrthos :: Orthos -> Orthos -> Orthos
-mergeOrthos = undefined
+mergeOrthos (Orthos o1) (Orthos o2) = Orthos $ Set.union o1 o2
 
-combineOver :: Config -> Orthos -> Ortho -> Orthos
-combineOver = undefined
+fromList :: [Ortho] -> Orthos
+fromList orthos = Orthos $ Set.fromList orthos
 
 pickOne :: Orthos -> (Ortho, Orthos)
-pickOne = undefined
+pickOne (Orthos o) = let
+  (f, s) = Set.deleteFindMin o
+  in (f, Orthos s)
 
-emptyOrthos :: Orthos -> Bool 
-emptyOrthos = undefined
+isEmpty :: Orthos -> Bool
+isEmpty (Orthos o)= Set.null o
 
-combineIfBase :: Config -> Orthos -> Ortho -> Orthos -- if it is not base return the empty orthos. If it is base return only previously unknown results
-combineIfBase = error "not implemented"
-
-eatWord :: Config -> Text -> Orthos
-eatWord = undefined
+eatWord :: Config -> String -> Orthos
+eatWord conf cur = Orthos $ Set.fromList $ fromAnswer <$> wordToUniqueAnswer conf cur
 
 fromAnswer :: Answer -> Ortho
-fromAnswer = undefined
+fromAnswer (Answer a b c d) = Ortho $ Set.fromList 
+  [Node a' $ Path [], 
+   Node b' $ Path [b'],
+   Node c' $ Path [c'], 
+   Node d' $ Path $ sort [b', c']] where 
+  a' = pack a
+  b' = pack b 
+  c' = pack c
+  d' = pack d
 
 emptyOrtho :: Orthos
-emptyOrtho = undefined
+emptyOrtho = Orthos Set.empty
 
 insert :: Orthos -> Ortho -> Orthos
-insert = undefined
+insert (Orthos s) o = Orthos $ Set.insert o s
 
 size :: Orthos -> Int
-size = undefined
+size (Orthos o) = Set.size o
+
+combineUpIfBase :: Config -> Orthos -> Ortho -> Orthos 
+combineUpIfBase c os o = if isNotBase o then emptyOrtho else let 
+  l = combineUpLeft c os o
+  r = combineUpRight c os o
+  in mergeOrthos l r
+
+-- this would be much faster if ortho were indexed by distance
+isNotBase :: Ortho -> Bool
+isNotBase (Ortho s) = let 
+  (Path underlying) = location $ maximumBy (compare `on` locationLength) $ Set.toList s
+  in nub underlying /= underlying
+
+locationLength :: Node -> Int
+locationLength (Node _ (Path l)) = Prelude.length l
+
+-- Reader would allow skipping this config pass
+combineUpRight :: Config -> Orthos -> Ortho -> Orthos
+combineUpRight c os o = let 
+  projects = projectsBackward c os o 
+  found = diagonalsLeft projects o
+  in filterOld os found
+
+filterOld :: Orthos -> Orthos -> Orthos
+filterOld (Orthos old) (Orthos new) = Orthos $ new `Set.difference` old
+
+combineUpLeft :: Config -> Orthos -> Ortho -> Orthos
+combineUpLeft c os o = let 
+  projects = projectsForward c os o 
+  found = diagonalsRight projects o
+  in filterOld os found
 
 projectsForward :: Config -> Orthos -> Ortho -> Orthos
 projectsForward = undefined
@@ -78,11 +116,8 @@ diagonalsLeft = undefined
 diagonalsRight :: Orthos -> Ortho -> Orthos
 diagonalsRight = undefined
 
-combineUpLeft :: Ortho -> Ortho -> Ortho
-combineUpLeft = undefined
-
-combineUpRight :: Ortho -> Ortho -> Ortho
-combineUpRight = undefined
+combineOver :: Config -> Orthos -> Ortho -> Orthos
+combineOver = undefined
 
 centersMatchLeft :: Orthos -> Ortho -> DirectedOrthos
 centersMatchLeft = undefined
