e3752ee459ea9079b8c3e6d1e9fab8f2506de60c
michaelrauh <michaelrauh@ymail.com>
2021-09-06 13:09:15 -0400

    WIP more thoughts. Deciding on ortho representation as list of set instead of as a set. Need to index orthos by origin. There will be a use for more metadata later with over

--- src/Ortho.hs
+++ src/Ortho.hs
@@ -9,6 +9,11 @@ import Data.List (delete, sort, maximumBy, nub, groupBy, permutations)
 import Data.Text
 import Data.Maybe
 
+
+-- TODO break file in three - ortho, orthos, fold
+-- TODO make ortho a list of set instead of a set
+-- TODO index orthos by origin
+
 newtype Path = Path {path :: [Text]} deriving (Eq, Ord, Show)
 data Node = Node
   { name :: Text,
@@ -41,12 +46,12 @@ sift config known increment = if isEmpty increment then known else let
 mergeOrthos :: Orthos -> Orthos -> Orthos
 mergeOrthos (Orthos o1) (Orthos o2) = Orthos $ Set.union o1 o2
 
-fromList :: [Ortho] -> Orthos -- export this and not the data constructor
+fromList :: [Ortho] -> Orthos
 fromList orthos = Orthos $ Set.fromList orthos
 
 pickOne :: Orthos -> (Ortho, Orthos)
 pickOne (Orthos o) = let
-  (f, s) = Set.deleteFindMin o -- at this time min is arbitrary but if ord were defined by distance, then min would iterate in order of distance
+  (f, s) = Set.deleteFindMin o
   in (f, Orthos s)
 
 isEmpty :: Orthos -> Bool
@@ -77,11 +82,15 @@ size (Orthos o) = Set.size o
 
 combineUpIfBase :: Config -> Orthos -> Ortho -> Orthos
 combineUpIfBase c os o = if isNotBase o then emptyOrtho else let
-  l = combineUpLeft c os o
-  r = combineUpRight c os o
+  newOs = selectSameDimensionalityForBaseOrtho os o
+  l = combineUpLeft c newOs o
+  r = combineUpRight c newOs o
   in mergeOrthos l r
 
--- this would be much faster if ortho were indexed by distance
+selectSameDimensionalityForBaseOrtho :: Orthos -> Ortho -> Orthos -- this assumes ortho is base dims and only selects based upon number of dims
+selectSameDimensionalityForBaseOrtho = error "not implemented"
+
+-- this would be much faster if ortho were indexed by distance. lookupMax would do it. alternatively inspect a mapping from axis name to length
 isNotBase :: Ortho -> Bool
 isNotBase (Ortho s) = let
   (Path underlying) = location $ maximumBy (compare `on` locationLength) $ Set.toList s
@@ -108,21 +117,27 @@ combineUpLeft c os o = let
 
 projectsForward :: Config -> Orthos -> Ortho -> Orthos
 projectsForward c os o = let
-  ns = nodes o
-  origin = Set.findMin $ Set.filter ((0 ==) . locationLength) ns -- this would be faster if indexed by distance. Alternatively if ord were distance findMin would work. This would also expose toAscList
-  forward = Map.findWithDefault Set.empty (unpack (name origin)) (next c) -- this unpack would be unneccesary if everything was text
+  forward = Map.findWithDefault Set.empty ((unpack . getName . getOrigin) o) (next c) -- this unpack would be unneccesary if everything was text
   matchingOrigin = findWithOrigin os forward
   axisCorrespondence = Prelude.concat $ findAxisCorrespondence c o <$> Ortho.toList matchingOrigin
   in Orthos $ Set.fromList (toOrtho <$> Prelude.filter (checkAllProjectionsExceptOriginAndHop c) axisCorrespondence)
 
+getName :: Node -> Text
+getName = name
+
+getOrigin :: Ortho -> Node
+getOrigin o = Set.findMin $ Set.filter ((0 ==) . locationLength) (nodes o) -- this would be faster if indexed by distance. Alternatively if ord were distance findMin would work
+
 checkAllProjectionsExceptOriginAndHop :: Config -> Correspondence -> Bool -- reader would jump config over this
 checkAllProjectionsExceptOriginAndHop c (Correspondence from to corr) = let
   corrMap = Map.fromList corr
-  nodesToCheckFilter = Set.filter ((<) 2 . locationLength) . nodes -- this would be simpler with distance bucketing. Potentially slower though.
-  nodesToCheckFrom = nodesToCheckFilter from
-  nodesToCheckTo = nodesToCheckFilter to
+  nodesToCheckFrom = getNodesOfDistanceGreaterThan 1 from
+  nodesToCheckTo = getNodesOfDistanceGreaterThan 1 to
   checksPassed = checkNodeProjectsForwardAcrossMappedPath c corrMap nodesToCheckTo nodesToCheckFrom
   in checksPassed
+
+getNodesOfDistanceGreaterThan :: Int -> Ortho -> Set.Set Node
+getNodesOfDistanceGreaterThan dist (Ortho nodes) = Set.filter ((> 1) . locationLength) nodes -- this would be simpler and faster with distance bucketing
 
 checkNodeProjectsForwardAcrossMappedPath :: Config -> Map String String -> Set Node -> Set Node -> Bool -- reader would jump config over this
 checkNodeProjectsForwardAcrossMappedPath c corrMap from to = let
@@ -130,10 +145,16 @@ checkNodeProjectsForwardAcrossMappedPath c corrMap from to = let
   in checkEachPairProjects c correspondingNodeNames
 
 findCorresponding :: Map String String -> Set Node -> Node -> (String, String) 
-findCorresponding corrMap toSet (Node fromName (Path fromLocation)) = let
-  toPath = Path (pack . (corrMap !) . unpack <$> fromLocation)
-  toNode = Set.findMin $ Set.filter ((toPath ==) . location) toSet -- this would be faster if there could be a mapping from path to name
-  in (unpack fromName, (unpack . name) toNode)
+findCorresponding corrMap toSet fromNode = let
+  toPath = Path (pack . (corrMap !) . unpack <$> getLocation fromNode)
+  toNode = findNodeWithPath toPath toSet
+  in (unpack (getName fromNode), (unpack . name) toNode)
+
+getLocation :: Node -> [Text]
+getLocation = error "not implemented"
+
+findNodeWithPath :: Path -> Set Node -> Node -- todo stop using Set Node. It can't be optimized. Give back a nodeset which is normally held by an ortho and is actually a list of set of node
+findNodeWithPath toPath toSet = Set.findMin $ Set.filter ((toPath ==) . location) toSet
 
 findAxisCorrespondence :: Config -> Ortho -> Ortho -> [Correspondence] -- there are less redundant ways to do this. If one axis fails it's a complete failure.
 findAxisCorrespondence c from to = let
